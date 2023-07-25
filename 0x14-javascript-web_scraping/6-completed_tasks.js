952ca01510807259500c1177174e9a9c34e2aa4c
sselimelsayed90 <saleemelsayed17@gmail.com>
2023-07-25 11:42:29 +0000

    selim

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-// The first argument is the API URL
-const baseURL = process.argv[2];
-request(baseURL, (error, response, body) => {
-  const aggregate = {};
-  if (error) {
-    console.log(error);
-  }
-  const json = JSON.parse(body);
-  json.forEach(element => {
-    if (element.completed) {
-      if (!aggregate[element.userId]) {
-        aggregate[element.userId] = 0;
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
       }
-      aggregate[element.userId]++;
+
+      if (completed) ++completedTasksByUsers[userId];
     }
-  });
-  console.log(aggregate);
+
+    console.log(completedTasksByUsers);
+  }
 });
